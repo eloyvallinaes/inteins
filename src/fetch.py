9198c4f1f9ad9b00e92043d54e69b532839a2dc5
evallina <eloyvallina33@gmail.com>
2022-06-15 18:36:17 +0200

    Track fetch module

--- src/fetch.py
+++ src/fetch.py
@@ -1,5 +1,6 @@
 """
-Fetch sequences datasets from different sources
+Classes for fetching sequence data from different sources.
+
 """
 
 import csv
@@ -10,9 +11,27 @@ from Bio import Entrez
 Entrez.email = "eloyvallina33@gmail.com"
 
 class RefSeqFetch:
+    """
+    Given RefSeq protein ideas, retrieve sequence and taxonomic information
+    over the NCBI Entrez interface.
+    """
     taxkeys = ["kingdom", "phylum", "subphylum", "class", "order", "family", "genus"]
     def set_ids(self, ids):
         self.ids = ids
+
+    def post(self):
+        handle = Entrez.epost("protein", id=",".join(self.ids))
+        return Entrez.read(handle).values()
+
+    def fetch(self):
+        query_key, WebEnv = self.post()
+        handle = Entrez.efetch(
+                        db="protein",
+                        query_key=query_key,
+                        WebEnv=WebEnv,
+                        retmode="xml"
+                        )
+        return handle
 
     def getdata(self):
         handle = self.fetch()
@@ -29,23 +48,10 @@ class RefSeqFetch:
             rows.append(row)
         return rows
 
-    def post(self):
-        handle = Entrez.epost("protein", id=",".join(self.ids))
-        return Entrez.read(handle).values()
-
-    def fetch(self):
-        query_key, WebEnv = self.post()
-        handle = Entrez.efetch(
-                        db="protein",
-                        query_key=query_key,
-                        WebEnv=WebEnv,
-                        retmode="xml"
-                        )
-        return handle
-
 
 class COGFetch:
     URL = "https://www.ncbi.nlm.nih.gov/research/cog/api/cog/?cog={}&format=json&page={}"
+
     def __init__(self, cogid: str, pagefrom=1, pageto=2, sequencer=RefSeqFetch()):
         self.cogid = cogid
         self.pagefrom = pagefrom
@@ -54,15 +60,15 @@ class COGFetch:
         self.records = self.getrecords()
         self.sequences = self.getseqdata()
 
-    def readpage(self, page):
+    def readpage(self, page) -> dict:
         """
-        Send a GET request to the COG database URL and returns the content as JSON.
+        Send a GET request to the COG database URL and return the content as JSON.
         """
         r = requests.get(COGFetch.URL.format(self.cogid, page))
         if r.ok:
             return r.json()
 
-    def readrange(self):
+    def readrange(self) -> list[dict]:
         """
         Visit the pages in range pagefrom->pageto and collect results in data attribute.
         """
@@ -76,7 +82,12 @@ class COGFetch:
                 break
         return data
 
-    def getrecords(self, filename=""):
+    def getrecords(self) -> list[dict]:
+        """
+        Slice the COG JSON data obtained from a range of pages and return
+        a list of dictionaries with fields of interest.
+        """
+
         records = []
         for record in self.readrange():
             records.append(
@@ -95,13 +106,21 @@ class COGFetch:
             )
         return records
 
-    def getseqdata(self):
+    def getseqdata(self) -> list[dict]:
+        """
+        Infer sequence accesions from COG data and obtaine sequences and
+        taxonomic categoricals
+        """
         ids = [r["refseq_accno"] for r in self.records]
         self.sequencer.set_ids(ids)
         return self.sequencer.getdata()
 
 
-    def writeout(self, filename=""):
+    def writeout(self, filename="") -> pd.DataFrame:
+        """
+        Merge sequence and COG data and return a single DataFrame. If filename,
+        suppres output and write dataframe to file.
+        """
         seqdf = pd.DataFrame(self.sequences)
         cogdf = pd.DataFrame(self.records)
         out = seqdf.merge(cogdf, on = "refseq_accno")
@@ -112,9 +131,6 @@ class COGFetch:
         return out
 
 
-
-
-
 if __name__ == '__main__':
-    fetcher = COGFetch("COG0417", pagefrom=1, pageto=2)
+    fetcher = COGFetch("COG0417")
     df = fetcher.writeout()
