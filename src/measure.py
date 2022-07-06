ba337eeb693ab5e0ac2945651ba6ad8e68cee3c3
evallina <eloyvallina33@gmail.com>
2022-07-06 18:34:43 +0200

    Abstract common methods to a Measure class

--- src/measure.py
+++ src/measure.py
@@ -7,7 +7,7 @@ import pandas as pd
 from parse import InterproSegments
 
 
-class MeasureCOG:
+class Measure:
     AA = 'ACDEFGHIKLMNPQRSTVWY'
     COLNAMES = [
         "length",
@@ -21,6 +21,52 @@ class MeasureCOG:
         "ncd1000",
     ]
 
+    @classmethod
+    def physcoprops(cls, seq, acc):
+        # aa composition
+        L = len(seq)
+        props = {f"{letter}": seq.count(letter) / L for letter in Measure.AA}
+        # accession
+        props['refseq_accno'] = acc
+        # properties
+        fPos = sum([seq.count(letter) for letter in "KR"]) / L
+        fNeg = sum([seq.count(letter) for letter in "DE"]) / L
+        fCharged = fPos + fNeg
+        fFatty = sum([seq.count(letter) for letter in "FLIV"]) / L
+        nc = seq.count("K") + seq.count("R") - seq.count("D") - seq.count("E")
+        mw = L * 110
+        sasa = 6.3 * mw**0.73
+        ncd1000 = nc / sasa * 1000
+        mwkda = mw / 1000
+
+        values = [L, sasa, mwkda, fCharged, fFatty, fPos, fNeg, nc, ncd1000]
+        props.update({
+            f"{key}": val
+            for key, val in zip(Measure.COLNAMES, values)
+        })
+        return props
+
+    def measure_inteins(self):
+        data = []
+        for acc, regions in self.regions.items():
+            for intein in regions['inteins']:
+                data.append(Measure.physcoprops(intein['seq'], acc))
+        return data
+
+    def measure_host(self):
+        data = []
+        for acc, regions in self.regions.items():
+            data.append(Measure.physcoprops(regions['host'], acc))
+        return data
+
+    def write_hostdata(self, outname):
+        pd.DataFrame(self.hostdata).to_csv(outname, index=False)
+
+    def write_inteindata(self, outname):
+        pd.DataFrame(self.inteindata).to_csv(outname, index=False)
+
+
+class MeasureCOG(Measure):
     def __init__(self, seqfile, ntermfile, ctermfile):
         self.sequences = pd.read_csv(seqfile, sep="\t")\
             .to_dict(orient="records")
@@ -61,50 +107,6 @@ class MeasureCOG:
             data[acc] = {'inteins': inteins, 'host': hostseq}
         return data
 
-    def measure_inteins(self):
-        data = []
-        for acc, regions in self.regions.items():
-            for intein in regions['inteins']:
-                data.append(Measure.physcoprops(intein['seq'], acc))
-        return data
-
-    def measure_host(self):
-        data = []
-        for acc, regions in self.regions.items():
-            data.append(Measure.physcoprops(regions['host'], acc))
-        return data
-
-    def write_hostdata(self, outname):
-        pd.DataFrame(self.hostdata).to_csv(outname, index=False)
-
-    def write_inteindata(self, outname):
-        pd.DataFrame(self.inteindata).to_csv(outname, index=False)
-
-    @classmethod
-    def physcoprops(cls, seq, acc):
-        # aa composition
-        L = len(seq)
-        props = {f"{letter}": seq.count(letter) / L for letter in Measure.AA}
-        # accession
-        props['refseq_accno'] = acc
-        # properties
-        fPos = sum([seq.count(letter) for letter in "KR"]) / L
-        fNeg = sum([seq.count(letter) for letter in "DE"]) / L
-        fCharged = fPos + fNeg
-        fFatty = sum([seq.count(letter) for letter in "FLIV"]) / L
-        nc = seq.count("K") + seq.count("R") - seq.count("D") - seq.count("E")
-        mw = L * 110
-        sasa = 6.3 * mw**0.73
-        ncd1000 = nc / sasa * 1000
-        mwkda = mw / 1000
-
-        values = [L, sasa, mwkda, fCharged, fFatty, fPos, fNeg, nc, ncd1000]
-        props.update({
-            f"{key}": val
-            for key, val in zip(Measure.COLNAMES, values)
-        })
-        return props
-
     @classmethod
     def extract_host(cls, sequence, inteins):
         indeces = [0]
@@ -133,61 +135,23 @@ class MeasureCOG:
         return inteins
 
 
-class MeasureInterpro:
-    AA = 'ACDEFGHIKLMNPQRSTVWY'
-    COLNAMES = [
-        "length",
-        "sasa",
-        "mwkda",
-        "fCharged",
-        "fFatty",
-        "fPos",
-        "fNeg",
-        "nc",
-        "ncd1000",
-    ]
-
+class MeasureInterpro(Measure):
     def __init__(self, fastafilename):
         self.regions = InterproSegments().parse(fastafilename)
         self.hostdata = self.measure_host()
         self.inteindata = self.measure_inteins()
 
-    @classmethod
-    def physcoprops(cls, seq, acc):
-        # aa composition
-        L = len(seq)
-        props = {f"{letter}": seq.count(letter) / L for letter in MeasureInterpro.AA}
-        # accession
-        props['refseq_accno'] = acc
-        # properties
-        fPos = sum([seq.count(letter) for letter in "KR"]) / L
-        fNeg = sum([seq.count(letter) for letter in "DE"]) / L
-        fCharged = fPos + fNeg
-        fFatty = sum([seq.count(letter) for letter in "FLIV"]) / L
-        nc = seq.count("K") + seq.count("R") - seq.count("D") - seq.count("E")
-        mw = L * 110
-        sasa = 6.3 * mw**0.73
-        ncd1000 = nc / sasa * 1000
-        mwkda = mw / 1000
-
-        values = [L, sasa, mwkda, fCharged, fFatty, fPos, fNeg, nc, ncd1000]
-        props.update({
-            f"{key}": val
-            for key, val in zip(MeasureInterpro.COLNAMES, values)
-        })
-        return props
-
     def measure_inteins(self):
         data = []
         for acc, regions in self.regions.items():
             for intein in regions['inteins']:
-                data.append(MeasureInterpro.physcoprops(intein['seq'], acc))
+                data.append(Measure.physcoprops(intein['seq'], acc))
         return data
 
     def measure_host(self):
         data = []
         for acc, regions in self.regions.items():
-            data.append(MeasureInterpro.physcoprops(regions['host'], acc))
+            data.append(Measure.physcoprops(regions['host'], acc))
         return data
 
     def write_hostdata(self, outname):
