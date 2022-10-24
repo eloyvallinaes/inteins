a5ebb4a5d39e3c94714ddd3096b2b94303e79c98
evallina <eloyvallina33@gmail.com>
2022-10-24 15:42:51 +0200

    Map lineages to unique taxids instead of every accession

--- scripts/extractTaxonomy.py
+++ scripts/extractTaxonomy.py
@@ -1,12 +1,14 @@
 # /usr/bin/python
 
 import csv
-import json
 import requests
-from requests.adapters import HTTPAdapter
 from pathlib import Path
+from requests.adapters import HTTPAdapter
+from time import sleep
 
-url = "https://rest.uniprot.org/uniprotkb/search?query={}&fields=lineage"
+from src import parse
+
+url = "https://www.ebi.ac.uk/proteins/api/taxonomy/lineage/{}"
 
 RANKS = [
     'superkingdom',
@@ -19,70 +21,54 @@ RANKS = [
 ]
 
 FIELDNAMES = RANKS.copy()
-FIELDNAMES.extend(['taxid', 'accession'])
+FIELDNAMES.append('taxid')
 
 
-def load(filename):
-    with open(filename, "r") as jsonfile:
-        j = json.load(jsonfile)
-        for result in j['results']:
-            yield result
-
-
-def taxonomy(result):
-    acc = result['to']['primaryAccession']
-    taxid = result['to']['organism']['taxonId']
+def taxonomy(taxid):
     s = requests.Session()
     a = requests.adapters.HTTPAdapter(max_retries=3)
     s.mount('http://', a)
-    r = s.get(url.format(acc))
+    r = s.get(url.format(taxid))
     if r.ok:
         data = r.json()
-        lineages = data["results"][0]["lineages"]
         row = {
             pair["rank"]: pair["scientificName"]
-            for pair in lineages if pair["rank"] in RANKS
+            for pair in data['taxonomies'] if pair["rank"] in RANKS
         }
-        row.update({
-            "accession": acc, "taxid": taxid
-        })
 
         return row
 
 
-def main(jsonname, outname):
-    with open(outname, "w") as outfile:
-        writer = csv.DictWriter(outfile, fieldnames=FIELDNAMES)
-        writer.writeheader()
-        for result in load(jsonname):
-            writer.writerow(taxonomy(result))
-
-
 if __name__ == '__main__':
-    jsonPath = Path("json")
+    fasta = Path("fasta/interpro")
     try:
         known = set([
             line.split(",")[0]
-            for line in open('ipr036844_taxonomy.csv', "r").readlines()
+            for line in open('taxonomy.csv', "r").readlines()
         ])
     except FileNotFoundError:
-        known = {}
+        known = set()
 
     rows = []
-    with open("ipr036844_taxonomy.csv", "a") as csvfile:
+    i = 0
+    with open("taxonomy.csv", "a") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
-        if len(known) > 0:
+        if len(known) == 0:
             writer.writeheader()
 
-        for result in load(jsonPath / "ipr036844_uniprot.json"):
-            acc = result['to']['primaryAccession']
-            if acc in known:
+        parser = parse.Fasta2Dict(fasta / "IPR036844.fasta")
+        taxids = set(parser.taxids.values())
+        for taxid in taxids:
+            if taxid in known:
                 continue
 
-            known.add(acc)
-
             try:
-                writer.writerow(taxonomy(result))
+                row = taxonomy(taxid)
+                row.update({"taxid": taxid})
+                writer.writerow(row)
 
             except KeyError:
                 print(f"{acc}: lineage not found")
+
+            known.add(taxid)
+            sleep(0.5)
