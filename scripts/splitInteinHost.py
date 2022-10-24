ea20a0f27f3d11411a63b39ec29d01dd21b81d2b
evallina <eloyvallina33@gmail.com>
2022-10-24 12:06:11 +0200

    Update to new parse module

--- scripts/splitInteinHost.py
+++ scripts/splitInteinHost.py
@@ -7,8 +7,8 @@ Split sequences into inteins and host proteins and write to FASTA file.
 from src import parse
 from pathlib import Path
 
-fasta = Path("fasta")
+fasta = Path("fasta/interpro")
 
-parser = parse.InterproSegments(fasta / "IPR036844.fasta")
-parser.to_fasta("host", fasta / "IPR036884_host.fasta")
-parser.to_fasta("intein", fasta / "IPR036884_intein.fasta")
+parser = parse.Fasta2Dict(fasta / "IPR036844.fasta")
+parser.write_segments(fasta / "inteins.fasta", "inteins")
+parser.write_segments(fasta / "hosts.fasta", "hosts")
